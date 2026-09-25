a6cc6d65ab3f65500d45a4bab7618c419d467027
Dimash <dinmukhamedappaz@gmail.com>
2026-09-25 16:33:32 +0500

    HW1: Task 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,3 @@
 fun main() {
-    println("Use the val keyword when the value doesn't change.")
-    println("Use the var keyword when the value can change.")
-    println("When you define a function, you define the parameters that can be passed to it.")
-    println("When you call a function, you pass arguments for the parameters.")
+    println("New chat message from a friend")
 }
