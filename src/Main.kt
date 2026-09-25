58fefa043654d0cd61f687d1e2e34bfa3d95d865
Dimash <dinmukhamedappaz@gmail.com>
2026-09-25 16:53:37 +0500

    HW1: Task 7

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,25 @@
 fun main() {
     val firstNumber = 10
     val secondNumber = 5
-    val result = firstNumber + secondNumber
+    val thirdNumber = 8
+
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = add(firstNumber, thirdNumber)
+
+    val subFirst = subtract(firstNumber, secondNumber)
+    val subSecond = subtract(firstNumber, thirdNumber)
 
     println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
+
+    println("$firstNumber - $secondNumber = $subFirst")
+    println("$firstNumber - $thirdNumber = $subSecond")
+}
+
+fun add(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
+fun subtract(num1: Int, num2: Int): Int {
+    return num1 - num2
 }
